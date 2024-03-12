037976020945b5e56463557ad05bbc89c4b8a18c
Dmitry Suprunov <dsuprunov@gmail.com>
2024-03-12 17:14:30 +0100

    Some minor updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
+from sample_package.sample_module import greeting
+
+
 def main() -> None:
-    print('Hello World!')
+    print(greeting())
+    print('Today is', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 
 if __name__ == '__main__':
